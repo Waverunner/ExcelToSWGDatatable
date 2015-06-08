22faa2aa6e8214e2f230d37b4380b5826579e2e7
Waverunner <lewaverunner@gmail.com>
2015-06-08 11:41:14 -0400

    Added visual interface for converting workbooks

---
+++ src/com/projectswg/tools/Main.java
@@ -0,0 +1,112 @@
+/*******************************************************************************
+ * Excel to SWG Iff Datatable
+ * Copyright (C) 2015  Waverunner
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ ******************************************************************************/
+
+package com.projectswg.tools;
+
+import com.projectswg.tools.controllers.MainController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+/**
+ * Created by Waverunner on 6/8/2015
+ */
+public class Main extends Application {
+    private static Main instance;
+    private MainController controller;
+    private Stage stage;
+
+    public static void main(String[] args) {
+        Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> {
+            throwable.printStackTrace();
+
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Exception");
+            alert.setHeaderText("An exception has occurred");
+            alert.setContentText(throwable.getLocalizedMessage());
+
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+
+            throwable.printStackTrace(pw);
+
+            String exceptionText = sw.toString();
+
+            Label label = new Label("The exception stacktrace was:");
+
+            TextArea textArea = new TextArea(exceptionText);
+            textArea.setEditable(false);
+            textArea.setWrapText(true);
+
+            textArea.setMaxWidth(Double.MAX_VALUE);
+            textArea.setMaxHeight(Double.MAX_VALUE);
+            GridPane.setVgrow(textArea, Priority.ALWAYS);
+            GridPane.setHgrow(textArea, Priority.ALWAYS);
+
+            GridPane expContent = new GridPane();
+            expContent.setMaxWidth(Double.MAX_VALUE);
+            expContent.add(label, 0, 0);
+            expContent.add(textArea, 0, 1);
+
+            alert.getDialogPane().setExpandableContent(expContent);
+
+            alert.showAndWait();
+        });
+        launch(args);
+    }
+
+    public static Main getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        instance = this;
+        stage = primaryStage;
+
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        try {
+            fxmlLoader.load(getClass().getResourceAsStream("ui/main.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        controller = fxmlLoader.getController();
+        Parent parent = fxmlLoader.getRoot();
+        Scene scene = new Scene(parent);
+
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public Stage getStage() {
+        return stage;
+    }
+}
---
+++ src/com/projectswg/tools/controllers/MainController.java
@@ -0,0 +1,162 @@
+/*******************************************************************************
+ * Excel to SWG Iff Datatable
+ * Copyright (C) 2015  Waverunner
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ ******************************************************************************/
+
+package com.projectswg.tools.controllers;
+
+import com.projectswg.tools.Main;
+import com.projectswg.tools.SwgExcelConverter;
+import com.projectswg.tools.libs.SWGFile;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.CheckBoxListCell;
+import javafx.stage.FileChooser;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Waverunner on 6/8/2015
+ */
+public class MainController implements Initializable {
+
+    @FXML
+    ListView<FileItem> listView;
+    @FXML
+    TextField directoryField;
+
+    ObservableList<FileItem> outputSelectionList;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        outputSelectionList = FXCollections.observableList(new ArrayList<>());
+        listView.setItems(outputSelectionList);
+        listView.setCellFactory(CheckBoxListCell.forListView(param -> param.getSelectedProperty()));
+    }
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        if (!(event.getSource() instanceof Button))
+            return;
+
+        Button button = (Button) event.getSource();
+
+        switch (button.getText()) {
+            case "Browse":
+                handleBrowseDirectory();
+                break;
+            case "Convert":
+                handleConvertSelections();
+                break;
+        }
+    }
+
+    private void handleBrowseDirectory() {
+        FileChooser fileChooser = new FileChooser();
+
+        fileChooser.setTitle("Select Worksheet to Convert");
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Excel Workbook", "*.xlsx", "*.xls"));
+        File workbook = fileChooser.showOpenDialog(Main.getInstance().getStage());
+        if (workbook == null)
+            return;
+        try {
+            handlePopulateList(workbook);
+        } catch (IOException | InvalidFormatException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleConvertSelections() {
+        SwgExcelConverter converter = new SwgExcelConverter();
+
+        ArrayList<FileItem> fileItems = new ArrayList<>(outputSelectionList.filtered(FileItem::isSelected));
+        fileItems.forEach(fileItem -> convertFileItem(fileItem, converter));
+    }
+
+    private void convertFileItem(FileItem item, SwgExcelConverter converter) {
+        SWGFile iff = converter.convert(item.getSheet());
+        if (iff != null) {
+            try {
+                iff.save(item.getFile());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void handlePopulateList(File source) throws IOException, InvalidFormatException {
+        Workbook workbook = WorkbookFactory.create(source);
+        for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
+            outputSelectionList.add(getFileItem(source.getAbsolutePath(), workbook.getSheetAt(0)));
+        }
+        workbook.close();
+    }
+
+    private FileItem getFileItem(String parent, Sheet sheet) {
+        String path = parent.substring(0, parent.lastIndexOf("\\") + 1) + sheet.getSheetName() + ".iff";
+        return new FileItem(new File(path), sheet);
+    }
+
+    private class FileItem {
+        private File file;
+        private Sheet sheet;
+        private BooleanProperty selectedProperty = new SimpleBooleanProperty(false);
+
+        public FileItem(File file, Sheet sheet) {
+            this.file = file;
+            this.sheet = sheet;
+        }
+
+        public final boolean isSelected() {
+            return selectedProperty.getValue();
+        }
+
+        public final File getFile() {
+            return file;
+        }
+
+        public Sheet getSheet() {
+            return sheet;
+        }
+
+        public final BooleanProperty getSelectedProperty() {
+            return selectedProperty;
+        }
+
+        @Override
+        public String toString() {
+            return sheet.getSheetName() + "\t--> " + file.getAbsolutePath();
+        }
+    }
+}
