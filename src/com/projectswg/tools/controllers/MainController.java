ffcacef9e44b57b0eb312539eefdeb71af6babd9
Waverunner <lewaverunner@gmail.com>
2015-06-08 12:28:58 -0400

    A dialog is now shown on conversion completion and the UI is refreshed

--- src/com/projectswg/tools/controllers/MainController.java
+++ src/com/projectswg/tools/controllers/MainController.java
@@ -29,6 +29,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
@@ -102,7 +103,22 @@ public class MainController implements Initializable {
         SwgExcelConverter converter = new SwgExcelConverter();
 
         ArrayList<FileItem> fileItems = new ArrayList<>(outputSelectionList.filtered(FileItem::isSelected));
-        fileItems.forEach(fileItem -> convertFileItem(fileItem, converter));
+
+        String text = "Finished creating:\n";
+
+        for (FileItem fileItem : fileItems) {
+            convertFileItem(fileItem, converter);
+            text += "\t" + fileItem.getFile().getAbsolutePath() + "\n";
+        }
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(Main.getInstance().getStage().getTitle());
+        alert.setHeaderText("Excel Conversion");
+        alert.setContentText(text);
+        alert.show();
+
+        directoryField.clear();
+        outputSelectionList.clear();
     }
 
     private void convertFileItem(FileItem item, SwgExcelConverter converter) {
